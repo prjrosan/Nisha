36f1cb5aacfd89bb7bb357d3552448f58b3efdd7
prjrosan <prajaroshan01@gmail.com>
2025-08-19 14:24:10 +0900

    Update render_build.sh

--- Nisha/render_build.sh
+++ Nisha/render_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
